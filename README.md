679a5dae61637c6d42d062ab644599b914e64791
KunalMehra075 <112753481+KunalMehra075@users.noreply.github.com>
2022-12-13 13:28:39 +0530

    dummy data fetching in index.js from db.json

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # urbane-look-6820
+
+1. npm install -g json-server
+2. npx json-server --watch db.json
+3. click on products, you can see the api products
---
+++ index.js
@@ -0,0 +1,14 @@
+// Check ReadME once
+
+let BaseUrl = "http://localhost:3000/Products";
+async function getData() {
+  try {
+    let res = await fetch(BaseUrl);
+    let data = await res.json(res);
+    console.log(data);
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+getData();
