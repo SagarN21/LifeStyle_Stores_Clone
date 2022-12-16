185f5746c1bf21215fff8432888287f68bb6a0d0
VivekTomar03 <tomarvivek90viv@gmail.com>
2022-12-16 21:59:18 +0530

    day all task done

--- index.js
+++ index.js
@@ -280,17 +280,3 @@ function scrollFunction() {
 }
 
 
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
-
-function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
-}
-
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("main").style.marginLeft= "0";
-}
